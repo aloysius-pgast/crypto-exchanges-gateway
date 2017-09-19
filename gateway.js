5160cbbe9e5328790bedbd840bb18ff30d9e13ae
Aloysius Pendergast <aloysius.pgast@gmail.com>
2017-09-19 10:08:43 +0200

    Log which services (ie: exchanges, pushover, coinmarket) are enabled upon startup

--- gateway.js
+++ gateway.js
@@ -58,28 +58,53 @@ if (!checker.check(config))
 }
 config = checker.getCfg();
 
+// add log if CoinMarketCap is enabled
+if (config.coinmarketcap.enabled)
+{
+    logger.warn("CoinMarketCap API is enabled");
+}
+
 //-- update config based on environment (used when using docker container)
 // check exchanges config
-_.forEach(['binance','bittrex','poloniex'], function(exchange) {
+_.forEach(config.exchanges, function(obj, exchange) {
     let key = process.env[util.format('cfg.exchanges.%s.key', exchange)];
     let secret = process.env[util.format('cfg.exchanges.%s.secret', exchange)];
     if (undefined !== key && '' != key && undefined !== secret && '' != secret)
     {
-        logger.warn("Enabling '%s' exchange...", exchange);
         config.exchanges[exchange]['key'] = key;
         config.exchanges[exchange]['secret'] = secret;
     }
 });
-// check pushover config
+// log which exchanges are enabled
+_.forEach(config.exchanges, function(obj, exchange) {
+    if (config.exchanges[exchange]['enabled'])
+    {
+        if ('' != config.exchanges[exchange]['key'] && '' != config.exchanges[exchange]['secret'])
+        {
+            logger.warn("%s exchange is enabled (public API & trading API)", exchange);
+        }
+        else
+        {
+            logger.warn("%s exchange is enabled (public API)", exchange);
+        }
+    }
+});
+
+//-- check pushover config
 let pushoverUser = process.env['cfg.pushover.user'];
 let pushoverToken = process.env['cfg.pushover.token'];
 if (undefined !== pushoverUser && '' != pushoverUser && undefined !== pushoverToken && '' != pushoverToken)
 {
-    logger.warn("Enabling 'PushOver' service...", exchange);
     config.pushover.enabled = true;
     config.pushover.user = pushoverUser;
     config.pushover.token = pushoverToken;
 }
+// add log if push over is enabled
+if (config.pushover.enabled && '' != config.pushover.user && '' != config.pushover.token)
+{
+    logger.warn("PushOver API is enabled");
+}
+
 // check config
 let logLevel = process.env['cfg.logLevel'];
 if (undefined !== logLevel)
