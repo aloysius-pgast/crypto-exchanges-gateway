96470b1f0682a5cdefb2beef592b1fb4973a2a70
Aloysius Pendergast <aloysius.pgast@gmail.com>
2017-11-13 11:40:47 +0100

    Add support for ApiKey in UI

--- gateway.js
+++ gateway.js
@@ -165,6 +165,7 @@ const app = express();
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "apikey");
     res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
     next();
 });
