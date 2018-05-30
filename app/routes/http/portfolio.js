c22092f31d8a127ce01423696f055c31d1169d6b
Aloysius Pendergast <aloysius.pgast@gmail.com>
2018-05-30 11:54:45 +0200

    When an exchange is in demo mode, only generate fake balances from top 20 CMC symbols

--- app/routes/http/portfolio.js
+++ app/routes/http/portfolio.js
@@ -15,11 +15,11 @@ _.forEach(serviceRegistry.getExchanges(), (obj, id) => {
         // use fakeExchange if demo mode is enabled for exchange
         if (obj.demo)
         {
-            exchanges[id] = new FakeExchangeClass(obj.instance);
+            exchanges[id] = {isDemo:true, instance:new FakeExchangeClass(obj.instance)};
         }
         else
         {
-            exchanges[id] = obj.instance;
+            exchanges[id] = {isDemo:false, instance:obj.instance};
         }
     }
 });
@@ -74,7 +74,15 @@ app.get('/portfolio', (req, res) => {
     }
     let arr = [];
     _.forEach(filteredList, (id) => {
-        let p = exchanges[id].getBalances();
+        let p;
+        if (exchanges[id].isDemo)
+        {
+            p = getDemoBalances(exchanges[id].instance);
+        }
+        else
+        {
+            p = exchanges[id].instance.getBalances();
+        }
         arr.push({promise:p, context:{exchange:id,api:'getBalances'}});
     });
     let balances = {};
@@ -93,20 +101,7 @@ app.get('/portfolio', (req, res) => {
                 balances[currency].volume += e.total;
             });
         });
-        let symbols = [];
-        _.forEach(Object.keys(balances), (s) => {
-            let symbol = s;
-            switch (s)
-            {
-                case 'IOTA':
-                    symbol = 'MIOTA';
-                    break;
-                case 'XRB':
-                    symbol = 'NANO';
-                    break;
-            }
-            symbols.push(symbol);
-        });
+        let symbols = mapExchangeCurrenciesToCoinMarketCapSymbol(Object.keys(balances));
         // get data from coinmarketcap
         coinmarketcap.getTickers({symbols:symbols}).then(function(data) {
             let tickers = {};
@@ -119,17 +114,10 @@ app.get('/portfolio', (req, res) => {
                 // ignore tickers we're not interested in
                 if (undefined === balances[entry.symbol])
                 {
-                    // try to map currency to coinmarketcap symbol
-                    switch (entry.symbol)
+                    entry.symbol = mapCoinMarketCapSymbolToExchangeCurrency(entry.symbol);
+                    if (undefined === balances[entry.symbol])
                     {
-                        case 'MIOTA':
-                            entry.symbol = 'IOTA';
-                            break;
-                        case 'NANO':
-                            entry.symbol = 'XRB';
-                            break;
-                        default:
-                            return;
+                        return;
                     }
                 }
                 tickers[entry.symbol] = entry.price_usd;
@@ -140,6 +128,40 @@ app.get('/portfolio', (req, res) => {
         });
     });
 });
+
+/**
+ * When exchange is in demo mode, only generate balances for symbols in the top 20
+ */
+const getDemoBalances = async (exchange) => {
+    let tickers = await coinmarketcap.getTickers({limit:20});
+    let currencies = _.map(tickers, (e) => {return e.symbol});
+    return exchange.getBalances(currencies);
+}
+
+const mapExchangeCurrenciesToCoinMarketCapSymbol = (list) => {
+    return _.map(list, (c) => {
+        switch (c)
+        {
+            case 'IOTA':
+                return 'MIOTA';
+            case 'XRB':
+                return 'NANO';
+        }
+        return c;
+    });
+}
+
+const mapCoinMarketCapSymbolToExchangeCurrency = (symbol) => {
+    // try to map currency to coinmarketcap symbol
+    switch (symbol)
+    {
+        case 'MIOTA':
+            return 'IOTA';
+        case 'NANO':
+            return 'XRB';
+    }
+    return symbol;
+}
 
 const sendPortfolio = (res, balances, tickers) => {
     let totalPrice = 0;
